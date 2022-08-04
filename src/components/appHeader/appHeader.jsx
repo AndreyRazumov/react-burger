23409934b7cd0fb7ad53544dd503102bc74b31f6
AndreyRazumov <dusha0001@yandex.ru>
2022-08-04 03:11:40 +0300

    Header

--- src/components/appHeader/appHeader.jsx
+++ src/components/appHeader/appHeader.jsx
@@ -8,13 +8,13 @@ export default class AppHeader extends React.Component {
             <header className={headerStyles.header}>
                 <nav className={`${headerStyles.navigation} pt-4 pb-4`}>
                 <li className='pt-4 pr-5 pb-4 pl-5'>
-                    <a href='#' className={`${headerStyles.linkActive} text text_type_main-default`}>
+                    <a href='https://' className={`${headerStyles.linkActive} text text_type_main-default`}>
                     <BurgerIcon type="primary" />
                     <span className='ml-2'>Конструктор</span>
                     </a>
                 </li>
                 <li className='pt-4 pr-5 pb-4 pl-5 ml-2'>
-                    <a href='#' className={`${headerStyles.link} text text_type_main-default`}>
+                    <a href='https://' className={`${headerStyles.link} text text_type_main-default`}>
                     <ListIcon type="secondary" />
                     <span className='ml-2'>Лента Заказов</span>
                     </a>
@@ -25,7 +25,7 @@ export default class AppHeader extends React.Component {
                 </div>                
                 <nav className={headerStyles.navigation}>
                     <li className='pt-4 pr-5 pb-4 pl-5'>
-                        <a href='#' className={`${headerStyles.link} text text_type_main-default`}>
+                        <a href='https://' className={`${headerStyles.link} text text_type_main-default`}>
                         <ProfileIcon type="secondary" />
                         <span className='ml-2'>Личный кабинет</span>
                         </a>
